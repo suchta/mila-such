4624551604697a5bf94b8f38d82a56b3b7986906
suchta <suchta@gmail.com>
2016-07-11 17:53:32 -0400

    Not happy with how I'm implementing ShopifyBuy API. Must fix shim

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import {Component} from '@angular/core';
 import {OnInit} from '@angular/core';
+//
+declare var ShopifyBuy: any;
+//
 // import {ROUTER_DIRECTIVES} from '@angular/router';
 
 // import {HomeComponent} from './home/home.component';
 // import {AboutComponent} from './about/about.component';
 import {TestComponent} from './test.component';
-
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles.css';
 
@@ -19,9 +21,30 @@ import '../styles.css';
 export class AppComponent implements OnInit{
   list: Number;
   names: String[];
+  shopifyClient: any;
   //
   ngOnInit() {
     this.list = 10;
     this.names = ['Joe','Omar','Mila','Katie','Molly','Chi','Frog','Fluffy'];
+    //
+
+    this.shopifyClient = ShopifyBuy.buildClient({
+      apiKey: '6559b53e69aeb0db205e92ef8f6c0703',
+      myShopifyDomain: 'mila-such',
+      appId: '6'
+    });
+    //this is my client, and it works well.
+    this.shopifyClient.fetchAllProducts().then(
+      function(value: Object) {
+        console.log("SUCCESS: " + value);
+        //alert(value.length);
+        //this.$q.defer().resolve();
+        //value => this.AllProducts = value;
+        //
+      },//success
+      function(reason: any) {
+        return null;
+      } // failure
+    );
   }
 }

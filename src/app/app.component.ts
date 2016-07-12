9be65bd40a836dc285dd8eb0026452fb8679e099
suchta <suchta@gmail.com>
2016-07-12 11:10:19 -0400

    Updated call to ShopifyBuy API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,6 +22,7 @@ export class AppComponent implements OnInit{
   list: Number;
   names: String[];
   shopifyClient: any;
+  productsArray: any;
   //
   ngOnInit() {
     this.list = 10;
@@ -35,16 +36,22 @@ export class AppComponent implements OnInit{
     });
     //this is my client, and it works well.
     this.shopifyClient.fetchAllProducts().then(
-      function(value: Object) {
-        console.log("SUCCESS: " + value);
+      function(value: any) {
+        //console.log("SUCCESS: " + value[0]);
         //alert(value.length);
         //this.$q.defer().resolve();
         //value => this.AllProducts = value;
         //
-      },//success
-      function(reason: any) {
-        return null;
-      } // failure
+        for(var i = 0; i < value.length; i++){
+          console.log(value[i].attrs.handle);
+          //this.productsArray.push(value[i].attrs.handle);
+        }
+        //this.names=>this.productsArray;
+      }
+      //success
+      // function(reason: any) {
+      //   return null;
+      // } // failure
     );
   }
 }
